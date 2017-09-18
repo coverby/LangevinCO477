e6dbd9ffab609d2d87e4e8cac9df8e19338c6125
Clyde Overby <coverby@ur.rochester.edu>
2017-09-18 16:56:16 -0400

    Install file added.  I think I did it correctly.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Langevin477CO',
+      version='.01',
+      description='CHE 477 Langevin Simulator',
+      author='Clyde Overby',
+      packages=['Langevin477CO'],
+     )
