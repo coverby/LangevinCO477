699fa4d5805a3a9d8a98f21b39bc049326b60dc0
Clyde Overby <coverby@ur.rochester.edu>
2017-09-20 11:33:10 -0400

    Added requirements, updated git ignore, added setup cfg

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+      long_description = ''.join(f.readlines())
+
 setup(name='Langevin477CO',
       version='.01',
       description='CHE 477 Langevin Simulator',
+      long_description=long_description,
       author='Clyde Overby',
-      packages=['Langevin477CO'],
+      packages=['Langevin477CO']
      )
---
+++ tests/test_lang.py
@@ -0,0 +1,1 @@
+import Langevin477CO
