6e1b8d2a6463c6dcfc0c02f2f9e80b271799cc30
Clyde Overby <coverby@ur.rochester.edu>
2017-09-27 11:44:11 -0400

    Visualization is working

--- Langevin477CO/lang.py
+++ Langevin477CO/lang.py
@@ -1,2 +1,45 @@
+import numpy as np
+from .visualization import SimVis, start_server
+import io
+import asyncio
+
+def read_energy(input_file):
+    #datafile = io.StringIO(input_file)
+    #print(input_file.getvalue())
+    pos = []
+    energy = []
+    data = np.loadtxt(input_file)
+    #print(data)
+    #print(data[1][1])
+    
+    for row in range(len(data)):   
+        pos.append(data[row][0])
+        energy.append(data[row][1]) 
+    return pos, energy
+
+#def start():
+#    print("LangevinCo477 start succeeded.")
+
+
+                
+async def main(sv):
+    #create a simple energy
+
+    x = np.linspace(-1, 1, 100)
+    y = x**2
+    sv.set_energy(x, y)
+
+
+
+    while True:
+        sv.set_position(np.random.random(1))
+        await asyncio.sleep(0.5)
+
+
+
 def start():
-    print("LangevinCo477 start succeeded.")
+    sv = SimVis()
+    start_server(sv)
+    asyncio.ensure_future(main(sv))
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
